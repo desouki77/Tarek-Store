708238c853ce0a827f8d500ea2339b9dc3b8e847
desouki77 <mahmoud.desouki77@gmail.com>
2024-10-19 00:52:44 +0300

    initial teh frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Your components will go here */}
+      <h1>مرحباً بك في تطبيق الطارق</h1>
+    </div>
+  );
+}
+
+export default App;
