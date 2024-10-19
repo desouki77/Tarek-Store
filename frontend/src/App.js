768e2d98456e08578317b328b61d0d1edb5e9203
desouki77 <mahmoud.desouki77@gmail.com>
2024-10-19 18:25:06 +0300

    update defult frontend configs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div>
-      {/* Your components will go here */}
+   
       <h1>مرحباً بك في تطبيق الطارق</h1>
     </div>
   );
