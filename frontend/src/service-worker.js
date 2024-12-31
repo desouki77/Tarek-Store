7645330e8f4f42b0fbf9b745db4c9bc6094758a0
Mahmoud Desouki <mahmoud.desouki77@gmail.com>
2024-12-31 22:36:49 +0200

    set PWA

---
+++ frontend/src/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/css/main.css",
+  "/static/js/bundle.js",
+  "/static/js/0.chunk.js",
+  "/static/js/main.chunk.js",
+];
+
+/* eslint-disable no-restricted-globals */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
